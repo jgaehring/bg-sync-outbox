bfa051d5265b9dbcafd871527e55057683dc4503
James Gaehring <gaehj457@gmail.com>
2018-01-08 14:56:59 -0500

    some cleanup, error handling, etc.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const OBJ_STORE_NAME = 'forms';
 const POST_URL = 'https://jsonplaceholder.typicode.com/posts'
 
 // Register Service Worker, if supported
-const useServiceWorker = (('serviceWorker' in navigator && 'SyncManager' in window))
-if (useServiceWorker) {
+const outboxIsSupported = (('serviceWorker' in navigator && 'SyncManager' in window))
+if (outboxIsSupported) {
   navigator.serviceWorker.register('sw.js')
   .then( () => console.log("Service Worker registered successfully!"))
   .catch( (err) => console.error(err));
@@ -14,7 +14,6 @@ if (useServiceWorker) {
 // Open a new db to use as the Outbox
 const openOutbox = () => {
   return new Promise(function(resolve, reject) {
-    let outboxDB;
     const request = indexedDB.open('outbox', IDB_VERS);
     request.onerror = function(event) {
       reject(event.target.errorcode);
@@ -24,7 +23,6 @@ const openOutbox = () => {
       resolve(this.result);
     };
     request.onupgradeneeded = function(event) {
-      // const db = event.target.result;
       const store = event.currentTarget.result.createObjectStore(
         OBJ_STORE_NAME, {keyPath:'id', autoIncrement: true}
       );
@@ -38,16 +36,19 @@ const form = document.querySelector('.form');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const formData = new FormData(form);
-  if (useServiceWorker) {
-    openOutbox().then(db => {
-      postDataToOutbox(formData, db);
-      navigator.serviceWorker.ready.then( (reg) => {
-        return reg.sync.register('outbox');
-      });
-    })
-  } else {
-    postDataToServer(formData);
-  }
+  postDataToServer(formData)
+  .then(response => console.log("Successfully posted to server!\nResponse: ", response))
+  .catch( (err) => {
+    if (outboxIsSupported) {
+      openOutbox()
+      .then(db => postDataToOutbox(db, formData))
+      .then(result => console.log(result))
+      .then(() => navigator.serviceWorker.ready)
+      .then( (reg) => reg.sync.register('outbox'));
+    } else {
+      console.error("Network down; outbox not supported.", err);
+    }
+  })
 });
 
 // Raw FormData object cannot be stored because it has method calls attached which IDB cannot store, so we'll only store the entries. The Service Worker will reconstruct a new FormData object using these key-value pairs before posting to server.
@@ -61,25 +62,35 @@ function unpackFormData(formData) {
 
 // Post data to server over network if Outbox fails; an identical function should be used by Service worker to eventually sync Outbox with server.
 const postDataToServer = (formData) => {
-  fetch(POST_URL, {
-    method: 'POST',
-    body: formData
-  })
-    .then(resp => resp.json())
-    .then(data => console.log("Response: ", data))
+  return new Promise(function(resolve, reject) {
+    fetch(POST_URL, {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      return response.json();
+    })
+    .then(response => resolve(response))
+    .catch(error => reject(error))
+  });
 };
 
 // Add Form Data to Outbox so Service Worker can post to server when it has connectivity.
-const postDataToOutbox = (formData, outboxDB) => {
-  const formArray = unpackFormData(formData);
-  console.log("Outbox Array: ", formArray);
-  const store = outboxDB.transaction(OBJ_STORE_NAME, 'readwrite').objectStore(OBJ_STORE_NAME);
-  console.log(store);
-  const request = store.add(...formArray);
-  request.onerror = function(event) {
-    console.error(event.target.errorcode)
-  };
-  request.onsuccess = function(event) {
-    console.log("Post Result: ", event.target.result)
-  };
+const postDataToOutbox = (db, formData) => {
+  return new Promise(function(resolve, reject) {
+    const formArray = unpackFormData(formData);
+    console.log("Outbox Array: ", formArray);
+    const store = db.transaction(OBJ_STORE_NAME, 'readwrite').objectStore(OBJ_STORE_NAME);
+    console.log(store);
+    const request = store.add(...formArray);
+    request.onerror = function(event) {
+      reject(new Error(event.target.errorcode))
+    };
+    request.onsuccess = function(event) {
+      resolve(event.target.result)
+    };
+  });
 };
